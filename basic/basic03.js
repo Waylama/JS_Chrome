1216da7acb6cf12ab65fb5359bee33545c0aef59
ITLAM <113187673+Waylama@users.noreply.github.com>
2023-01-26 19:51:17 +0900

    basic contents

---
+++ basic/basic03.js
@@ -0,0 +1,15 @@
+const age = parseInt(prompt("How old are you?"));
+
+
+
+if(isNaN(age)||age<0){
+	console.log("Please write a real positive number");
+} else if(age < 18){
+	console.log("You can not drink!");
+} else if(age>=18 && age < 60) {
+	console.log("You can drink");
+} else {
+	console.log("You can drink but be careful");
+}
+
+
